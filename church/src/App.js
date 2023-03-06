19da9d59ecee38bfd2a62283d9c3af15cc8be01f
Kwabena Ababio Adwabour <adwabour@gmail.com>
2023-03-06 22:40:07 +0100

    Created the base for the react version of the project with tailwind

---
+++ church/src/App.js
@@ -0,0 +1,12 @@
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="font-bold text-7xl text-gray-700">testing</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ church/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    colors: {
+      "light": {
+        100: "#FAFBFB",
+      },
+      "dark":{
+        100:"#06051a"
+      },
+      'churchGreen':{
+        80:"#5c9c93",
+        100:"#1e6554",
+        200:"#227668",
+        250:"#37524f"
+      }
+    }
+  },
+  plugins: [],
+}
